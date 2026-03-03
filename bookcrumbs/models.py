5aa5150ff11fdab8d7b85610dd322f2108d7b2d4
ranjith.j <ranjithjacob.work@gmail.com>
2026-03-03 08:19:35 +0000

    adds base+index templates; adds Book model

--- bookcrumbs/models.py
+++ bookcrumbs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    image = models.URLField(max_length=250)
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=50)
+    series = models.CharField(max_length=200)
+    genre = models.CharField(max_length=50)
+    published_year = models.IntegerField()
+    character = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)

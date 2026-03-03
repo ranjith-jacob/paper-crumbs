93089dc4ca18760e71285cfb3851f7fe4ccae58c
ranjith.j <ranjithjacob.work@gmail.com>
2026-03-03 09:32:04 +0000

    adds navbar; builds book index and adds template

--- bookcrumbs/views.py
+++ bookcrumbs/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def home(request):
     return render(request, "index.html")
+
+def book_index(request):
+    books = Book.objects.all()
+    return render(request, "books/book_list.html", {"books": books})

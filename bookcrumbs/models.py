9c01c6e54d571ce54434d5e14b6b5694b0d479e3
ranjith.j <ranjithjacob.work@gmail.com>
2026-03-03 09:03:15 +0000

    modifies Book model for better default values

--- bookcrumbs/models.py
+++ bookcrumbs/models.py
@@ -2,11 +2,23 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    image = models.URLField(max_length=250)
+    image = models.URLField(
+        max_length=250,
+        default="https://images.pexels.com/photos/1333742/pexels-photo-1333742.jpeg",
+    )
     name = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
     series = models.CharField(max_length=200)
     genre = models.CharField(max_length=50)
     published_year = models.IntegerField()
-    characters = models.CharField(max_length=100)
-    locations = models.CharField(max_length=200)
+    characters = models.CharField(
+        max_length=100,
+        default="N/A",
+    )
+    locations = models.CharField(
+        max_length=200,
+        default="N/A",
+    )
+
+    def __str__(self):
+        return f"{self.name} by {self.author} | {self.series} | {self.published_year}"
